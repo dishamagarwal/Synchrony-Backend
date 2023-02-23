b34b699f69aecc48ece72f9380ceaf2f8fc927ca
Disha Agarwal <68860743+dishamagarwal@users.noreply.github.com>
2023-02-23 10:45:46 -0500

    removing comments

--- src/main/java/com/synchrony/springboot/UserController.java
+++ src/main/java/com/synchrony/springboot/UserController.java
@@ -34,40 +34,4 @@ public class UserController {
     public void deleteUserById(@PathVariable("id") int id) {
         userService.deleteUserById(id);
     }
-
-    // @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
-    // User getUser(@PathVariable Integer id){
-    //     return  userService.findById(id).get();
-    // }
-
-    // @RequestMapping(value = "/user", method = RequestMethod.POST)
-    // String addUser(@RequestBody String username, @RequestBody String password, 
-    // @RequestBody(required=false) String firstname, @RequestBody(required=false) String lastname, 
-    // @RequestBody(required=false) String phone){
-    //     User user = new User(username, password);
-    //     if (firstname != null || firstname != "") {
-    //         user.setFirstName(firstname);
-    //     }
-    //     if (lastname != null || lastname != "") {
-    //         user.setLastName(lastname);
-    //     }
-    //     if (phone != null || phone != "") {
-    //         user.setPhone(phone);
-    //     }
-    //     // TODO: adding user to database
-    //     try {
-    //         userService.createUser(user);
-    //     } catch(Exception e) {
-    //         System.out.println(e);
-    //     }
-    //     return "SUCCESS";
-    // }
-
-    
-
-    // @GetMapping("image/{id}")
-    // public Image getImage(@PathVariable("id") String id) {
-    //     LOGGER.info("getImage called for id {}", id);
-    //     return imageService.getImage(id);
-    // }
 }
